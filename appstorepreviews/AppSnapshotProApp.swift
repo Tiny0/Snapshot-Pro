758a75f91163e168b8ac200b427fdeda04e6d165
GitHub Action <action@github.com>
2026-09-17 13:44:16 +0000

    Sync from private repository: 2026-09-17 13:44:16

--- appstorepreviews/AppSnapshotProApp.swift
+++ appstorepreviews/AppSnapshotProApp.swift
@@ -1,8 +1,15 @@
 import SwiftUI
 import SwiftData
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
 @main
 struct AppSnapshotProApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     let container: ModelContainer
 
     init() {
